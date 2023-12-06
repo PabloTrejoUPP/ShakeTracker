6859c0f79752a0d7aeb487f4a698750620ae6dc4
ChArLyPeReZCP <153005074+ChArLyPeReZCP@users.noreply.github.com>
2023-12-06 11:57:03 -0600

    Add files via upload

---
+++ site.js
@@ -0,0 +1,11 @@
+﻿// Scripts JavaScript para funcionalidades interactivas (si es necesario)
+// Por ejemplo: desplazamiento suave al hacer clic en los enlaces del menú
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
